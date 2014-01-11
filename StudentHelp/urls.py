2a12f53ad4888d25509646d27e4691b3713ecca9
multiservicio <lusy97@gmail.com>
2014-01-11 17:13:51 +0200

    First stable state with views connected using templates using bootstrap

--- StudentHelp/urls.py
+++ StudentHelp/urls.py
@@ -6,5 +6,9 @@ from StudentHelp import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^(?P<palabra>\d+)/$', views.word, name='word')
+                       url(r'^quiz/$', views.quiz, name='quiz'),
+                       url(r'^search_form/$', views.search_form, name='search_form'),
+                       url(r'^display_meta/$', views.display_meta, name='display_meta'),
+                       url(r'^search/$', views.search, name='search'),
+                       url(r'^(?P<palabra>\d+)/$', views.word, name='word'),
 )
--- StudentHelp/views.py
+++ StudentHelp/views.py
@@ -14,4 +14,48 @@ def index(request):
 
 
 def word(request, palabra):
-    return HttpResponse("The word is %s." % palabra)
+    ua = request.META.get('HTTP_USER_AGENT', 'unknown')
+    return HttpResponse("Welcome to the page %s" % request.path + ", \nthe word is %s." % palabra +
+                        " and more information %s" % ua)
+
+
+def display_meta(request):
+    template = loader.get_template('StudentHelp/about.html')
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    c = RequestContext(request, {
+        'data': html
+    })
+    #return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    return HttpResponse(template.render(c))
+
+
+def search_form(request):
+    template = loader.get_template('StudentHelp/search.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+def quiz(request):
+    template = loader.get_template('StudentHelp/questionnaire.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+
+def search2(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
+
+
+def search(request):
+    template = loader.get_template('StudentHelp/results.html')
+    q = request.GET['q']
+    context = RequestContext(request, {
+        'searched': q
+    })
+    return HttpResponse(template.render(context))
