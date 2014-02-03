6b541e999158031b108ad9c75fbe4c03e173c842
multiservicio <lusy97@gmail.com>
2014-02-03 22:58:00 +0200

    Add Bleach library modify the parser to clean html using BeautifulSoup and Bleach

--- README.md
+++ README.md
@@ -10,5 +10,8 @@
     http://www.crummy.com/software/BeautifulSoup/
 * Request
     http://docs.python-requests.org/en/latest/
+* Bleach
+    https://pypi.python.org/pypi/bleach
+
 
 Pedro Diaz
--- StudentHelp/services/verbix.py
+++ StudentHelp/services/verbix.py
@@ -3,38 +3,47 @@ __author__ = 'pedro'
 from django.db import models
 from bs4 import BeautifulSoup
 import requests
+import bleach
 
 
 class Verbix(models.Model):
     service_name = "verbix"
     verb = ""
     baseUrl = "http://www.verbix.com/webverbix/"
+    url = ""
     language = "Finnish"
+    allowed_tags = ['class', 'td', 'tr', 'table', 'p', 'br', 'b', 'th']
 
     def get_conjugations(self, verb):
         """
         Constructor of the class
         """
         self.verb = verb
-        url = self.baseUrl + self.language + "/" + verb + ".html"
-        return self.parse_html(self.get_html(url))
+        self.url = self.baseUrl + self.language + "/" + verb + ".html"
+        return self.parse_html(self.get_html())
 
-    def get_html(self, url):
+    def get_html(self):
         """
         Get the whole HTML response from the service.
         Make it ready to be parsed
         """
-        html = requests.get(url)
+        html = requests.get(self.url)
         return html.text
 
     def parse_html(self, html):
         """
         Given an HTML code parse it and retrieve the results
         """
+        results = []
         soup = BeautifulSoup(html)
-        print(soup)
         tables = soup.find_all("table", class_="verbtable")
-        return tables[0]
+        for table in tables:
+            table['class'] = 'table'
+            sections = table.find_all('tr')
+            for section in sections:
+                clean_rows = bleach.clean(section, self.allowed_tags, strip=True)
+                results.append(clean_rows)
+        return results
 
     def __unicode__(self):
         return self.service_name
